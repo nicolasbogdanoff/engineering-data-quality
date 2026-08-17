df6686fe67eb541cc09878959603566ba928bb5c
Nicolás Mauricio Bogdanoff <nicolasbogdanoff@gmail.com>
2026-08-17 14:43:45 -0300

    Expose the CSV profiling helper

--- engineering_data_quality/__init__.py
+++ engineering_data_quality/__init__.py
@@ -1,3 +1,3 @@
-from .quality import ValidationIssue, profile_frame, validate_subgroups
+from .quality import ValidationIssue, profile_csv, profile_frame, validate_subgroups
 
-__all__ = ["ValidationIssue", "profile_frame", "validate_subgroups"]
+__all__ = ["ValidationIssue", "profile_csv", "profile_frame", "validate_subgroups"]
